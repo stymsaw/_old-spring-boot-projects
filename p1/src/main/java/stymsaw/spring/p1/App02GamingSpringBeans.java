344753bcf252158e528cade36253f3c1de60f6be
Satyam Saw <satyamkumarsaw30@gmail.com>
2024-02-02 15:22:37 +0530

    created first component that creates the object automatically

--- p1/src/main/java/stymsaw/spring/p1/App02GamingSpringBeans.java
+++ p1/src/main/java/stymsaw/spring/p1/App02GamingSpringBeans.java
@@ -1,22 +1,34 @@
 package stymsaw.spring.p1;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import stymsaw.spring.p1.game.GameRunner;
 import stymsaw.spring.p1.game.GamingConsole;
 import stymsaw.spring.p1.game.MarioGame;
-import stymsaw.spring.p1.game.SuperContraGame;
+import stymsaw.spring.p1.game.PacmanGame;
 
+
+@Configuration
+@ComponentScan("stymsaw.spring.p1.game")
 public class App02GamingSpringBeans {
+
+
+
+    @Bean
+    public GameRunner gameRunner(GamingConsole gamingConsole) {
+        return new GameRunner(gamingConsole);
+    }
 
     public static void main(String[] args) {
 
 
-        try (var context = new AnnotationConfigApplicationContext(GameConfiguration.class)) {
+        try (var context = new AnnotationConfigApplicationContext(App02GamingSpringBeans.class)) {
 
             context.getBean(GamingConsole.class).up();
 
             context.getBean(GameRunner.class).run();
-
 
 
         }
